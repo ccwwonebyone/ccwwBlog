fc0e8b55458ac700f28cc786b8b751de07ff25b7
ccwwonebyone <ccwwonebyone@163.com>
2018-05-22 15:54:04 +0800

    登陆页面调整

--- fornt/src/view/Login.vue
+++ fornt/src/view/Login.vue
@@ -1,6 +1,6 @@
 <template>
-<div >
-  <el-form ref="form" :model="form" id="loginForm" style="background-color: #fff;width: 300px;">
+<div id="login-form">
+  <el-form ref="form" :model="form">
     <el-form-item>
       <el-input v-model="form.username" placeholder="用户名"></el-input>
     </el-form-item>
@@ -9,7 +9,7 @@
     </el-form-item>
     <el-form-item>
       <el-button type="text">忘记密码？</el-button>
-      <el-button type="primary">登录</el-button>
+      <el-button type="primary" style="margin-left: 214px;" v-on:click="login">登录</el-button>
     </el-form-item>
   </el-form>
 </div>
@@ -19,18 +19,15 @@
     data() {
       return {
         form: {
-          username: 'admin',
+          username: '',
           password: ''
         },
         dom:{
-          loginForm:'loginForm'
+          loginForm:'login-form'
         }
       }
     },
     methods: {
-      onSubmit() {
-        console.log('submit!');
-      },
       vetical:function(elId){
         var screen_height   = document.documentElement.clientHeight;
         var screen_width    = document.documentElement.clientWidth;
@@ -38,8 +35,26 @@
         var el_height       = el.clientHeight;
         var el_width        = el.clientWidth;
         console.log(screen_height,screen_width,el_height,el_width);
-        el.style.marginTop  = (screen_width - el_height)/2 + 'px';
-        el.style.marginLeft = (screen_height - el_width)/2 + 'px';
+        el.style.marginTop  = (screen_height - el_height)/2 + 'px';
+        el.style.marginLeft = (screen_width - el_width)/2 + 'px';
+      },
+      login:function(){
+        this.$axios({
+          method:'post',
+          url:'user/login',
+          data:this.form
+        })
+        .then(response => {
+          if(response.data.code == '200'){
+            var type = 'success';
+          }else{
+            var type = 'error';
+          }
+          this.$message({
+            message: response.data.message,
+            type: type
+          });
+        });
       }
     },
     mounted:function(){       //挂载完成时
@@ -50,55 +65,16 @@
        }
     }
   }
-
-	/*export default{
-		name:'login',
-		data(){
-			return {
-				info:'ManPro',
-        message:'',
-        alert:false,
-        dom:{
-            loginForm:'loginForm'
-        },
-        loginInfo:{
-            username:'admin',
-            password:''
-        },
-			}
-		},
-		methods:{
-			vetical:function(elId){
-				var screen_height = document.documentElement.clientHeight;
-        var el = document.getElementById(elId);
-        var el_height = el.clientHeight;
-        el.style.marginTop = (screen_height - el_height)/2 + 'px';
-			},
-      login:function(){
-        var self = this;
-        this.$axios({
-          method:'post',
-          url:'/user/login',
-          data:this.loginInfo
-        })
-        .then(function(response){
-          self.alert = true;
-          self.message = response.data.message;
-        });
-      },
-      close_alert:function(){
-        this.alert = false;
-      }
-		},
-		mounted:function(){       //挂载完成时
-			var self = this;
-      self.vetical(self.dom.loginForm);
-      window.onresize = function(){
-          self.vetical(self.dom.loginForm);
-       }
-		}
-	}*/
 </script>
 <style type="text/css">
-
+  body{
+    background-color: #242c39;
+  }
+  #login-form{
+    width: 360px;
+    height: 200px;
+    background-color: #fff;
+    padding: 45px 30px 0 30px;
+    border-radius:5px;
+  }
 </style>
