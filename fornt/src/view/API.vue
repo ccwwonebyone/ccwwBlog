0c62e1447d51e4ba00e6245947f9445e04310211
ccwwonebyone <ccwwonebyone@163.com>
2018-06-04 19:35:54 +0800

    API页面

--- fornt/src/view/API.vue
+++ fornt/src/view/API.vue
@@ -45,9 +45,10 @@
     </el-table-column>
     <el-table-column label="描述">
       <template slot-scope="scope">
-        <el-input placeholder="描述" v-model="scope.row.detail" v-on:mouseover.native="showdel(scope.$index)"
-         v-on:mouseout.native="hiddendel(scope.$index)">
-          <template slot="append"><i class="el-icon-delete" v-if="scope.row.showdel"></i></template>
+        <el-input placeholder="描述" v-model="scope.row.detail"
+          v-on:mouseover.native="showdel(scope.$index)"
+          v-on:mouseout.native ="hiddendel(scope.$index)">
+          <template slot="append"><el-button slot="append" icon="el-icon-delete" v-on:click.native="removeParam(scope.$index)" v-if="scope.row.showdel"></el-button></template>
         </el-input>
       </template>
     </el-table-column>
@@ -78,27 +79,27 @@
       }
     },
     methods: {
-      active:function(item) {
+      active:function(item) {   //请求方式
         this.request.active = item;
       },
-      showParams:function(){
+      showParams:function(){     //显示参数列表
         this.params.show = this.params.show ? false : true;
       },
-      showdel:function(index){
-        console.log(index,this.params.data.length-1);
+      showdel:function(index){      //显示删除按钮
         if(index != this.params.data.length-1) this.params.data[index].showdel = true;
       },
-      hiddendel:function(index){
-        console.log(index,this.params.data.length-1);
+      hiddendel:function(index){    //隐藏删除按钮
         if(index != this.params.data.length-1) this.params.data[index].showdel = false;
+      },
+      removeParam:function(index){    //移除参数
+        this.params.data.splice(index,1);
       }
     },
     watch:{
-      params:{
+      params:{    //参数列表的数据变化
         handler(val,oldVal){
           var data = val.data[val.data.length-1];
-          console.log(data)
-          if(data.key || data.value || data.detail) this.params.data.push({key:'',value:'',detail:''});
+          if(data.key || data.value || data.detail) this.params.data.push({key:'',value:'',detail:'',showdel:false});
         },
         deep:true
       }
