cda7787dab1b5805e73eaa48a5a58203ecabaa27
ccwwonebyone <ccwwonebyone@163.com>
2019-08-19 16:44:37 +0800

    数据库修正

--- ccwwBlog.sql
+++ ccwwBlog.sql
@@ -1,24 +1,8 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 80012
- Source Host           : localhost
- Source Database       : blog
-
- Target Server Type    : MySQL
- Target Server Version : 80012
- File Encoding         : utf-8
-
- Date: 09/21/2018 10:26:42 AM
-*/
-
-SET NAMES utf8;
+SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
---  Table structure for `one_article_tag`
+-- Table structure for one_article_tag
 -- ----------------------------
 DROP TABLE IF EXISTS `one_article_tag`;
 CREATE TABLE `one_article_tag` (
@@ -28,7 +12,7 @@ CREATE TABLE `one_article_tag` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `one_articles`
+-- Table structure for one_articles
 -- ----------------------------
 DROP TABLE IF EXISTS `one_articles`;
 CREATE TABLE `one_articles` (
@@ -39,20 +23,21 @@ CREATE TABLE `one_articles` (
   `markdown` text COMMENT 'markdown内容',
   `author` varchar(30) NOT NULL DEFAULT '' COMMENT '作者',
   `category_id` int(11) DEFAULT NULL COMMENT '类别',
+  `sort` int(11) DEFAULT '0' COMMENT '排序',
   `create_time` int(11) DEFAULT NULL,
   `update_time` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `one_articles`
+-- Records of one_articles
 -- ----------------------------
 BEGIN;
-INSERT INTO `one_articles` VALUES ('1', '欢迎使用ccwwBlog!', '', '<h1><a id=\"ccwwBlog_0\"></a>ccwwBlog</h1>\n<pre><code class=\"lang-\">简单的博客系统\n添加文章前先增加分类哦！\n</code></pre>\n', '# ccwwBlog\n\n```\n简单的博客系统\n添加文章前先增加分类哦！\n```', 'ccwwonebyone', '0', '1537496592', '1537496592');
+INSERT INTO `one_articles` VALUES (1, '欢迎使用ccwwBlog!', '', '<h1><a id=\"ccwwBlog_0\"></a>ccwwBlog</h1>\n<pre><code class=\"lang-\">简单的博客系统\n添加文章前先增加分类哦！\n</code></pre>\n', '# ccwwBlog\n\n```\n简单的博客系统\n添加文章前先增加分类哦！\n```', 'ccwwonebyone', 0, 0, 1537496592, 1537496592);
 COMMIT;
 
 -- ----------------------------
---  Table structure for `one_category`
+-- Table structure for one_category
 -- ----------------------------
 DROP TABLE IF EXISTS `one_category`;
 CREATE TABLE `one_category` (
@@ -68,7 +53,14 @@ CREATE TABLE `one_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `one_company`
+-- Records of one_category
+-- ----------------------------
+BEGIN;
+INSERT INTO `one_category` VALUES (1, 'php', '', 0, 1, 1, 1566203917, 1566203917);
+COMMIT;
+
+-- ----------------------------
+-- Table structure for one_company
 -- ----------------------------
 DROP TABLE IF EXISTS `one_company`;
 CREATE TABLE `one_company` (
@@ -83,17 +75,32 @@ CREATE TABLE `one_company` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Records of `one_company`
+-- Records of one_company
 -- ----------------------------
 BEGIN;
-INSERT INTO `one_company` VALUES ('1', 'ccwwBlog', 'ccwwBlog', 'ccwwblog', '/img/ca297c9c8010afeb9fa4aaf0fa2cbb.png', '/img/eddff169cdd12e3d6a663d7b7ed40b.jpg', 'ccwwBlog轻巧型博客系统', '/img/eddff169cdd12e3d6a663d7b7ed40b.jpg', null, null);
+INSERT INTO `one_company` VALUES (1, 'ccwwBlog', 'ccwwBlog', 'ccwwblog', '/img/ca297c9c8010afeb9fa4aaf0fa2cbb.png', '/img/eddff169cdd12e3d6a663d7b7ed40b.jpg', 'ccwwBlog轻巧型博客系统', '/img/eddff169cdd12e3d6a663d7b7ed40b.jpg', NULL, NULL);
 COMMIT;
 
 -- ----------------------------
---  Table structure for `one_tags`
+-- Table structure for one_medias
+-- ----------------------------
+DROP TABLE IF EXISTS `one_medias`;
+CREATE TABLE `one_medias` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) DEFAULT NULL COMMENT '文件地址',
+  `name` varchar(255) DEFAULT NULL COMMENT '文件名称',
+  `type` varchar(30) DEFAULT NULL COMMENT '文件类型',
+  `size` float(11,0) DEFAULT NULL COMMENT '文件大小',
+  `create_time` int(11) DEFAULT NULL,
+  `update_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for one_tags
 -- ----------------------------
 DROP TABLE IF EXISTS `one_tags`;
 CREATE TABLE `one_tags` (
@@ -105,7 +112,7 @@ CREATE TABLE `one_tags` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
---  Table structure for `one_users`
+-- Table structure for one_users
 -- ----------------------------
 DROP TABLE IF EXISTS `one_users`;
 CREATE TABLE `one_users` (
@@ -118,22 +125,5 @@ CREATE TABLE `one_users` (
   `update_time` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-SET FOREIGN_KEY_CHECKS = 1;
-
--- ----------------------------
---  Table structure for `one_medias`
--- ----------------------------
-DROP TABLE IF EXISTS `one_medias`;
-CREATE TABLE `one_medias` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `url` varchar(255) DEFAULT NULL COMMENT '文件地址',
-  `name` varchar(255) DEFAULT NULL COMMENT '文件名称',
-  `type` varchar(30) DEFAULT NULL COMMENT '文件类型',
-  `size` float(11,0) DEFAULT NULL COMMENT '文件大小',
-  `create_time` int(11) DEFAULT NULL,
-  `update_time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
