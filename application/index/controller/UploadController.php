4602e13f93a74861144cb477d8f699e36ee60477
ccwwonebyone <ccwwonebyone@163.com>
2020-05-26 10:37:44 +0800

    上传service调整

--- application/index/controller/UploadController.php
+++ application/index/controller/UploadController.php
@@ -2,26 +2,20 @@
 
 namespace app\index\controller;
 
-use think\Request;
 use app\index\service\MediaService;
+use think\Request;
 
 class UploadController extends Controller
 {
-    protected $mediaService;
     //验证规则
     protected $rules = [
-        'file|文件' =>'require',
+        'file|文件' => 'require',
     ];
-
-    public function _initialize()
-    {
-        $this->mediaService = new MediaService();
-    }
 
     public function upload(Request $request)
     {
         $file = $request->file('file');
-        $res = $this->mediaService->store($file);
-        return $res ? $this->asJson($res) : $this->asJson('','上传文件失败',422);
+        $res = MediaService::getSingletonInstance()->store($file);
+        return $res ? $this->asJson($res) : $this->asJson('', '上传文件失败', 422);
     }
 }
